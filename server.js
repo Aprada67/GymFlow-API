9d2051316ee0cabfdfdaa57512e24fa53b990eb4
Alberto Prada <albertojprada10@gmail.com>
2025-10-04 14:10:10 +0100

    finish swagger

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const port = process.env.PORT || 3000;
 
+// Middleware CORS
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+// Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// API Routes
 app.use('/', require('./routes'));
 
-
+// Database conection
 mongodb.initDb((err) => {
     if (err) {
         console.log(err);
     } else {
-        app.listen(port, () => {
-            console.log(`Database is listening and node running on port ${port}`);
-        });
+        app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
     }
-});
+})
