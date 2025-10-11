0bca8b0b2072f563626d3334a85fee5bbf59afbc
Alberto Prada <albertojprada10@gmail.com>
2025-10-11 13:44:15 +0200

    finish authenticate

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 const router = require('express').Router();
-
-router.use('/', require('./swagger'));
-
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const passport = require('passport');
 
 router.use('/users', require('./users'));
 router.use('/classes', require('./classes'));
 router.use('/memberships', require('./memberships'));
 router.use('/bookings', require('./bookings'));
+router.use('/', require('./swagger'));
+
+// router.get('/', (req, res) => {
+    //#swagger.tags=['Hello World]
+    //res.send('Hello World');
+//});
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    })
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,32 +4,85 @@ const mongodb = require('./data/database');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
 // Middleware CORS
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
+app
+    .use(bodyParser.json())
+    .use(session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+    }))
+    // This is the basic express session({...}) initialization.
+    .use(passport.initialize())
+    // Init passport on every route call.
+    .use(passport.session())
+    // Allow passport to use "express-session".
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
+        );
+        res.setHeader(
+            'Access-Control-Allow-Methods',
+            'POST, GET, PUT, PATCH, OPTIONS, DELETE'
+        );
+        next();
+    })
+    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+    .use(cors({ origin: '*' }))
+    .use('/', require('./routes/index'));
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+    function (accesToken, refreshToken, profile, done) {
+        // User.findOrCreate({ githubId: profileId }, function (err, user) {
+        return done(null, profile)
+        // });
+    }
+));
+
+// Passport Serializer
+passport.serializeUser((user, done) => {
+    done(null, user);
 });
 
+// Passport Deserializer
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out') });
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false
+}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+    });
+
 // Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // API Routes
-app.use('/', require('./routes'));
+// app.use('/', require('./routes'));
 
 // Database conection
 mongodb.initDb((err) => {
     if (err) {
         console.log(err);
     } else {
-        app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
+        app.listen(port, () => { console.log(`Database is listening and node running on port ${port}`) });
     }
 })
